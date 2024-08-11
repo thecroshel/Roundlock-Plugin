bab3780ec5bae72ad30d19c9092d26e2d8201a33
thecroshel <burak21efee@gmail.com>
2024-08-11 22:51:41 +0300

    Add files via upload

---
+++ RoundlockPlugin.cs
@@ -0,0 +1,45 @@
+using System;
+using Exiled.API.Features;
+using Exiled.API.Interfaces;
+using Server = Exiled.Events.Handlers.Server;
+
+namespace RoundlockPlugin
+{
+    public class RoundlockPlugin : Plugin<Config>
+    {
+        public static RoundlockPlugin Instance;
+
+        public override string Name => "RoundlockPlugin";
+        public override string Author => "thecroshel";
+        public override string Prefix => "roundlock_plugin";
+        public override Version Version => new Version(1, 0, 0);
+        public override Version RequiredExiledVersion => new Version(8, 4, 3);
+
+        public override void OnEnabled()
+        {
+            Instance = this;
+            Server.RoundStarted += OnRoundStarted;
+            base.OnEnabled();
+            Log.Info("RoundlockPlugin etkin.");
+        }
+
+        public override void OnDisabled()
+        {
+            Server.RoundStarted -= OnRoundStarted;
+            base.OnDisabled();
+            Log.Info("RoundlockPlugin devre dışı.");
+        }
+
+        private void OnRoundStarted()
+        {
+            Round.IsLocked = true;
+            Log.Info("Roundlock etkinleştirildi.");
+        }
+    }
+
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false; // Implemented the missing Debug property
+    }
+}
